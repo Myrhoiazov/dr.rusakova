38e4a581ce26aa71e01a961ab2749b58b422040c
Myrhoiazov <denis.mirgoyazov@gmail.com>
2024-11-03 00:45:12 +0100

    Delete folder shared/styles

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter} from 'react-router-dom';
 import AppRouter from './providers/router/ui/AppRouter';
+import './styles/app.scss';
 
 export const App = () => {
 	return (
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from 'app/App';
 import '@splidejs/react-splide/css';
-import './shared/styles/index.scss';
+// import './shared/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
